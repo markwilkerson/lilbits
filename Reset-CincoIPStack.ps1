5e869b0eb42f9b2683e3495b6807cd04e1a64e8b
Slab Bulkchest <41293910+markwilkerson@users.noreply.github.com>
2019-01-28 16:36:31 -0500

    Add files via upload

---
+++ Reset-CincoIPStack.ps1
@@ -0,0 +1,9 @@
+﻿$NIC_CARD = Get-NetIPAddress -AddressFamily IPv4 -InterfaceAlias "Ethernet*";
+
+Remove-NetIPAddress -IPAddress $NIC_CARD.IPAddress -Confirm:$false
+
+Remove-NetRoute -InterfaceAlias $NIC_CARD.InterfaceAlias -DestinationPrefix 0.0.0.0/0 -Confirm:$false
+
+Set-DnsClientServerAddress -InterfaceIndex $NIC_CARD.InterfaceIndex -ResetServerAddresses
+
+ipconfig /all
